ae780bd0950b2e55ec272e720de20205c7d41adb
Mario Van Raemdonck <mariovr5@hotmail.com>
2016-01-21 15:56:22 +0100

    made plotfunctions more general and flexibel

--- rundir/plotfunctions.py
+++ rundir/plotfunctions.py
@@ -36,8 +36,8 @@ import filecollector as fc
 ## for Palatino and other serif fonts use:
 #matplotlib.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']}) #adjust fonts
 #matplotlib.rc('font',**{'family':'serif','serif':['Palatino']})
-matplotlib.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-matplotlib.rc('text', usetex=True)
+#matplotlib.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+#matplotlib.rc('text', usetex=True)
 
 def makemovie(name = None):
   # makes a movie from all the .png files in the current directory
@@ -76,9 +76,10 @@ class Plot_Files(object):
         elif os.path.isfile(name):
             self.readdata([name], regexp = regexp)
 
-        self.fig = pl.figure(1,figsize=(8,6), dpi=100 , frameon = True , facecolor = '0.75' , edgecolor = 'w')
-        self.fig.add_subplot(111 , axisbg = 'w' , projection = 'rectilinear') #if you want to add axes on particular place: fig.add_axes([0.15, 0.1, 0.7, 0.3]) where ->  [begin , bottom to start axes , width , height ]
+        self.fig = pl.figure(1,figsize=(18,14), dpi=100 , frameon = True , facecolor = '0.75' , edgecolor = 'w')
+        self.fig.add_subplot('111' , axisbg = 'w' , projection = 'rectilinear') #if you want to add axes on particular place: fig.add_axes([0.15, 0.1, 0.7, 0.3]) where ->  [begin , bottom to start axes , width , height ]
         self.separated = False #if we have a list and need to plot the plots separated
+        self.axnum = 0
   
     def add_axes(self,pos = [0.5 , 0.2 , 0.4 , 0.3], axisbg = None , projection = 'rectilinear'):
         self.fig.add_axes(pos , axisbg = axisbg, projection = projection)
@@ -111,7 +112,7 @@ class Plot_Files(object):
       filecol = fc.File_Collector(dirname , search , notsearch = notsearch ,filelist = filelist , sortfunction = sortfunction , rev =rev )
       self.readdata(filecol.plotfiles, regexp = regexp ,  substr = substr, filename = filename)
   
-    def generate_plot(self, xlimg = None , ylimg =None , exname = '' , prefix = True, titel = None ,name = 'energyplot', depcol = 0, ylist = None , save = True, color = ['','','','','','','','','', '','', '' , '' , '' , ''], exdir = './', samedir = False, datanum = 0):
+    def generate_plot(self, xlimg = None , ylimg =None , exname = '' , prefix = True, titel = None ,name = 'energyplot', depcol = 0, ylist = None , save = True, color = ['','','','','','','','','', '','', '' , '' , '' , ''], exdir = './', samedir = False, datanum = 0, label = None):
       """
       Some nice plots to visualize the data with matplotlib.
       """
@@ -122,14 +123,18 @@ class Plot_Files(object):
         ylist = range(0,self.data[0].data.shape[1]) 
         ylist.remove(depcol)
         
+
       for i in ylist:
-          self.plotwrap(depcol, i, 'energy' , name =  exname, xlim = xlimg , ylim = ylimg , prefix = prefix ,save = False, label = self.data[datanum].columnheader[i], color = color[i-1], datanum = datanum)
+          if label == None:
+              label = self.data[datanum].columnheader[i]
+          self.plotwrap(depcol, i, 'energy' , name =  exname, xlim = xlimg , ylim = ylimg , prefix = prefix ,save = False, label = label , color = color[i-1], datanum = datanum)
       self.layout(self.data[datanum].get_xlabel() , self.data[datanum].get_ylabel(), tit = titel, xlim = xlimg , ylim = ylimg )
       if save:
           self.savefig(name+exname,  prefix = prefix, exdir = exdir, samedir =  samedir)  
   
     def plotwrap(self, xindex, yindex, yas, name = None, titel = None ,color = 'r' , sort = '' , label = None , xlim = None , ylim = None , prefix = False,save = True, datanum = 0):
-      self.fig.axes[0].plot(self.data[datanum].data[:len(self.data[datanum].data[:,yindex]),xindex],self.data[datanum].data[:,yindex], color+sort , label = label)
+      self.fig.axes[self.axnum].plot(self.data[datanum].data[:len(self.data[datanum].data[:,yindex]),xindex],self.data[datanum].data[:,yindex], color+sort , label = label)
+      #self.fig.axes[self.axnum].scatter(self.data[datanum].data[:len(self.data[datanum].data[:-3,yindex]),xindex],self.data[datanum].data[:-3,yindex])
       if self.separated == True and save:
         self.layout(self.data[datanum].get_xlabel(), self.data[datanum].get_ylabel(), tit = titel, xlim = xlim , ylim = ylim)
         self.savefig(name, filenum = i , prefix = prefix)
@@ -137,9 +142,9 @@ class Plot_Files(object):
         self.layout(self.data[datanum].get_xlabel() , self.data[datanum].get_ylabel(), tit = titel, xlim = xlim , ylim = ylim)
         self.savefig(name + 'together' , prefix = prefix)
   
-    def scatterplot(self ,  xvars , yvars , colorvars , colormap = 'hot' ):
+    def scatterplot(self ,  xvars , yvars , colorvars , colormap = None ):
         cm = pl.cm.get_cmap(colormap)
-        sc = self.fig.axes[0].scatter(xvars ,yvars, c=colorvars, cmap = cm )
+        sc = self.fig.axes[self.axnum].scatter(xvars ,yvars, c=colorvars, cmap = cm )
         pl.colorbar(sc)
   
     def normalize_to_groundstate(self):
@@ -156,38 +161,51 @@ class Plot_Files(object):
         self.data[i].data[:,1:3] = self.data[i].data[:,1:3] - gronddat[:,1:3] #we made sure that the data of the groundstateenergy is first in the rgdata list
       del(self.data[0].data, self.prefix[0])
       
-    def layout(self ,  xlab , ylab , xlim = None , ylim = None , tit = None , axnum = 0 , legendhand = None , legendlab = None , legendpos = 0 , finetuning = False , axbg = None , fs = 22, ticksize = 14):
+    def layout(self ,  xlab , ylab , xlim = None , ylim = None , tit = None , legendhand = None , legendlab = None , legendpos = 0 , finetuning = False , axbg = None , fs = 22, ticksize = 14):
       """
       In this function we finetune some aspects of the axes for all the tuning possibilitys see: http://matplotlib.org/api/axes_api.html
       especially the set functions ;)
       """
       print('We are starting with the layout')
-      self.fig.axes[axnum].set_xlabel(xlab, fontsize = fs)
-      self.fig.axes[axnum].set_ylabel(ylab , fontsize = fs)
+      self.fig.axes[self.axnum].set_xlabel(xlab, fontsize = fs)
+      self.fig.axes[self.axnum].set_ylabel(ylab , fontsize = fs)
       if xlim != None:
-        self.fig.axes[axnum].set_xlim(xlim) #good value for xlim in the case of a xi path is : (2*self.rgeq.energiel[0]-5*(self.rgeq.energiel[1]-self.rgeq.energiel[0]),2*self.rgeq.energiel[-1]+0.5) 
+        self.fig.axes[self.axnum].set_xlim(xlim) #good value for xlim in the case of a xi path is : (2*self.rgeq.energiel[0]-5*(self.rgeq.energiel[1]-self.rgeq.energiel[0]),2*self.rgeq.energiel[-1]+0.5) 
       if ylim != None:
-        self.fig.axes[axnum].set_ylim(ylim)
+        self.fig.axes[self.axnum].set_ylim(ylim)
       if tit != None:
-        self.fig.axes[axnum].set_title(tit , fontsize = fs)
+        self.fig.axes[self.axnum].set_title(tit , fontsize = fs)
       if legendlab != None:
-        self.fig.axes[axnum].legend(legendhand , legendlab, loc = legendpos)  #if you want to add extra info
+        self.fig.axes[self.axnum].legend(legendhand , legendlab, loc = legendpos)  #if you want to add extra info
       
-      #self.fig.axes[axnum].ticklabel_format(style='sci', axis='y') #force scientifique notation for y axis
-      #self.fig.axes[axnum].yaxis.major.formatter.set_powerlimits((0,0))
-      for tick in self.fig.axes[axnum].xaxis.get_major_ticks():
+      #self.fig.axes[self.axnum].ticklabel_format(style='sci', axis='y') #force scientifique notation for y axis
+      #self.fig.axes[self.axnum].yaxis.major.formatter.set_powerlimits((0,0))
+      self.fig.axes[self.axnum].yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter(useOffset=False))
+      #self.fig.axes[self.axnum].yaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%.1f'))
+      self.fig.axes[self.axnum].xaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%.1f'))
+      #self.fig.axes[self.axnum].locator_params(tight=True, nbins=3)
+      #self.fig.axes[self.axnum].xaxis.major.locator.set_params(nbins=3)
+
+
+      if self.axnum > 3:
+          self.fig.axes[self.axnum].yaxis.major.locator.set_params(nbins=3) 
+
+      for tick in self.fig.axes[self.axnum].xaxis.get_major_ticks():
         tick.label.set_fontsize(ticksize) 
-      for tick in self.fig.axes[axnum].yaxis.get_major_ticks():
+      for tick in self.fig.axes[self.axnum].yaxis.get_major_ticks():
         tick.label.set_fontsize(ticksize) 
+    
+    
+      self.fig.subplots_adjust(hspace=.5)
   
-      leg = self.fig.axes[axnum].legend(loc = legendpos) #draws the legend on axes[axnum] all the plots that you labeled are now depicted in legend
+      leg = self.fig.axes[self.axnum].legend(loc = legendpos) #draws the legend on axes[axnum] all the plots that you labeled are now depicted in legend
   
       if axbg != None:
-        self.fig.axes[axnum].set_axis_bgcolor(axbg)
+        self.fig.axes[self.axnum].set_axis_bgcolor(axbg)
       """
-      if you forgot to add a label to a line with linenumber: lnum you can do: self.fig.axes[axnum].lines[lnum].set_label('this is my new label')
-      the underneath is the same as : h , l = self.fig.axes[axnum].get_legend_handles_labels()
-                                      self.fig.axes[axnum].legend(h,l)
+      if you forgot to add a label to a line with linenumber: lnum you can do: self.fig.axes[self.axnum].lines[lnum].set_label('this is my new label')
+      the underneath is the same as : h , l = self.fig.axes[self.axnum].get_legend_handles_labels()
+                                      self.fig.axes[self.axnum].legend(h,l)
       """
       if finetuning == True:
         # the matplotlib.patches.Rectangle instance surrounding the legend
@@ -212,7 +230,9 @@ class Plot_Files(object):
       figname = self.savestring(name , filenum , samedir = samedir , prefix = prefix , exdir = exdir, typ = typ)
       self.fig.savefig(figname , dpi = 300 , facecolor = 'w' , edgecolor = 'w')
       self.fig.clf()
-      self.fig.add_subplot(111 , axisbg = 'w' , projection = 'rectilinear')
+      #self.axnum +=1
+      #self.fig.add_subplot('32%i' % (self.axnum+1) , axisbg = 'w' , projection = 'rectilinear')
+      self.fig.add_subplot('111' , axisbg = 'w' , projection = 'rectilinear')
   
     def savestring(self , name , filenum , samedir = False , prefix = True, exdir = './', filenumwrite = False, typ = 'png'):
       """
@@ -233,7 +253,7 @@ class Plot_Files(object):
         return '%s%s.%s' %(exdir, name, typ)
   
     def writetext(self ,text , pos , axnum = 0, hor = None  ,ver = None , rot = None ,fs =14 , transform = None):
-      self.fig.axes[axnum].text(pos[0] ,pos[1] ,text , rotation = rot ,horizontalalignment = hor, verticalalignment = ver , fontsize = fs, transform = transform) #, color = 'black', style = 'italic')
+      self.fig.axes[self.axnum].text(pos[0] ,pos[1] ,text , rotation = rot ,horizontalalignment = hor, verticalalignment = ver , fontsize = fs, transform = transform) #, color = 'black', style = 'italic')
   
     def least_sqr_fit(self,x, y):
       """
@@ -269,6 +289,7 @@ class Plot_Files(object):
         else:
             self.data[which].data[:,col] = [self.convert_ang_to_au(x)  for x in self.data[which].data[:,col]]
 
+
 def main():
     angstrom = False 
     fname = "results/beh2_sto_3g_symcompc1/beh2fcidocicompc1_sto-3g.dat"
@@ -284,22 +305,39 @@ def main():
     fname = 'results/h26-31gdoci/h26-31gdoci_6-31g.dat'
     fname = 'results/h2cc-ptzgdoci/h2cc-pqzdocinew_cc-pvtz.dat'
     fname = 'beh2testdatamoihiallopen.dat'
+    fname = 'results/beh2localtest/beh2localtest_sto-3g.dat'
+    fname = 'results/noconstraineddmhermextrema/noconstrainedm_.dat'
+    #fname = 'results/noplusconstraineddmhermextrema100angstrom/noconstrainedm_.dat'
+    fname = 'results/noconstraineddmhermextrema100angstromgoodtrans/noconstrainedm_.dat'
+    #fname = 'results/noconstraineddmhermextrema0.1angstrom/noconstrainedm_.dat'
+    #fname = 'results/noconstraineddmhermextremainfinitedistance/noconstrainedm_.dat'
+    fname = 'results/noconstraineddmhermextremainfiniteangstromscan7.98/noconstrainedm_.dat'
     
     plotter = Plot_Files(fname)
-    plotter.data[0].depvar['yas'] = 'Energy'#change the future y-axis label 
-    plotter.data[0].depvar['xas'] = '$R$'#change the future y-axis label 
-    plotter.data[0].units['x'] = r'(\AA)'
-    plotter.data[0].units['y'] = r'(E$_h$)'
-    title = 'BeH$_2$ (STO-3G)'
+    #title = 'NO$^+$ Infinite Angstrom scan different ham (STO-3G)'
+    title = 'NO$^+$ Infinite Angstrom scan 7.98 (STO-3G)'
     xlim = None
     ylim = None
-    #xlim = (0.4,3.5)
-    #ylim = ( -1.2 , -0.9) 
+    #xlim = (0.5,3.5)
+    #ylim = ( -15.9 , -14.5) 
+
     if angstrom:
         plotter.reader.convert(0) #converts angstrom to au.
         plotter.reader.units['x'] = '(a.u.)'
-    plotter.generate_plot(xlimg = xlim, ylimg = ylim, ylist = [1], titel = title, name = 'plot' , exname = 'short')
-    #plotter.generate_plot(xlimg = None, ylimg = None, ylist = [2,4,6], titel = title, name = 'overlapplot' ,  exname = 'sym')
+
+    plotter.data[0].depvar['xas'] = 'lambda ' #change the future y-axis label 
+    plotter.data[0].depvar['yas'] = 'FCI energy' #change the future y-axis label 
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [2], titel = title, name = 'plot' , exname = 'lambdascanfci')
+
+    #plotter.data[0].depvar['xas'] = 'Mulliken charge on N'#change the future y-axis label 
+    #plotter.data[0].depvar['yas'] = 'Extremum lambda'#change the future y-axis label 
+    #plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [3], titel = title, name = 'plot' , exname = 'mullikenlambda')
+    #plotter.data[0].depvar['yas'] = 'Energy'#change the future y-axis label 
+    #plotter.data[0].units['y'] = r'(E$_h$)'
+    #plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [2], titel = title, name = 'plot' , exname = 'mullikenenergy')
+    #plotter.data[0].depvar['xas'] = 'Extremum lambda'#change the future y-axis label 
+    #plotter.data[0].units['x'] = r'(a.u.)'
+    #plotter.generate_plot(depcol = 3 , xlimg = xlim, ylimg = ylim, ylist = [2], titel = title, name = 'plot' , exname = 'lambdaenergy')
     option = None
     #here under is an example to show the flexibility of the plotclass
     if option == 'inset':
@@ -320,7 +358,56 @@ def main():
         #see also: http://scipy-lectures.github.io/intro/matplotlib/matplotlib.html#ticks
         plotter.plotrgvars(begin = begin , stop =stop, axnum = 1)
         #plotter.separated = True
-    
+
+def togethermulliken():
+    fname = 'results/noconstraineddmhermextrema100angstromgoodtrans/noconstrainedm_.dat'
+    fname2 = 'results/noconstraineddmhermextremainfinitedistance/noconstrainedm_.dat'
+    fname3 = 'results/noconstraineddmhermextrema0.1angstrom/noconstrainedm_.dat'
+    fname4 = 'results/noconstraineddmhermextrema/noconstrainedm_.dat'
+
+    plotter = Plot_Files([fname, fname2, fname3 , fname4])
+    title = 'NO$^+$ Infinite Angstrom, 100angstrom (STO-3G)'
+    xlim = None
+    ylim = None
+
+    plotter.data[0].depvar['xas'] = 'Mulliken charge on N'#change the future y-axis label 
+    plotter.data[0].depvar['yas'] = 'Lambda at extremum'#change the future y-axis label 
+    plotter.data[0].units['y'] = r'(a.u.)'
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [3], titel = title, name = 'plot' , exname = 'mullikenlambdatogether', save = False, label = '100angstrom')
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [3], titel = title, name = 'plot' , exname = 'mullikenlambdatogether', save = False, datanum = 1, label = 'infinite distance')
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [3], titel = title, name = 'plot' , exname = 'mullikenlambdatogether', save = False, datanum = 2, label = '0.1 angstrom')
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [3], titel = title, name = 'plot' , exname = 'mullikenlambdatogether', save = True, datanum = 3, label = '1.225 angstrom')
+    plotter.data[0].depvar['yas'] = 'Energy'#change the future y-axis label 
+    plotter.data[0].units['y'] = r'(E$_h$)'
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [2], titel = title, name = 'plot' , exname = 'mullikenenergytogether', save = False, label = '100 angstrom')
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [2], titel = title, name = 'plot' , exname = 'mullikenenergytogether', save = False, datanum = 1, label = 'infinite distance' )
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [2], titel = title, name = 'plot' , exname = 'mullikenenergytogether', save = False, datanum =2, label = '0.1 angstrom' )
+    plotter.generate_plot(depcol = 0 , xlimg = xlim, ylimg = ylim, ylist = [2], titel = title, name = 'plot' , exname = 'mullikenenergytogether', save = True, datanum = 3, label = '1.225 angstrom')
+
+
+def shannon_scatter():
+    #fname = '/home/mario/DOCI-results/shannon_scattereq/shannon_scattereq_sto-3g.dat'
+    fname = './results/shannon_plots/shannontotsenioritybeh2sto-3g.dat'
+    #fname = 'results/shannon_scattereq6-31gfcivec/shannon_scattereq6-31gexcitation_6-31g.dat'
+    plotter = Plot_Files(fname)
+    plotter.data[0].depvar['yas'] = 'Energy'#change the future y-axis label 
+    plotter.data[0].depvar['xas'] = '$I_c$'#change the future y-axis label 
+    plotter.data[0].units['x'] = r''
+    plotter.data[0].units['y'] = r'(E$_h$)'
+    xlim = (0. , 10.)
+    ylim = (None , None)
+    #excitations = ['S','D','T','Q','5']
+    seniorities = ['0','2','4','6']
+    for i in range(2,5):
+    #for i in range(2,7):
+        #title = 'CI' +  ''.join(excitations[:i-1])
+        title = 'SEN' +  ''.join(seniorities[:i-1])
+        #plotter.generate_plot(xlimg = xlim, ylimg = ylim, ylist = [i], titel = title, name = 'plot' , exname = 'excitationscattereqsto-3g', exdir= '', prefix = False)
+        plotter.generate_plot(xlimg = xlim, ylimg = ylim, ylist = [i], titel = title, name = 'plot' , exname = 'seniorityscattereqsto-3g', exdir= '', prefix = True)
+
+
 if __name__ == '__main__':
-    main()  
+    #main()  
     #makemovie()
+    #shannon_scatter()
+    togethermulliken()
